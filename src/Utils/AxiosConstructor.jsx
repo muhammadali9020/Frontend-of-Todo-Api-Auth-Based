af91ae47ce04e05e12f0de03ace44c1973ba3178
Muhammad Ali <muhammadali271252@gmail.com>
2026-06-12 12:21:41 +0500

    Add token header to Axios instance

--- src/Utils/AxiosConstructor.jsx
+++ src/Utils/AxiosConstructor.jsx
@@ -1,11 +1,11 @@
 const APIURL=import.meta.env.VITE_API_URL
 import axios from "axios";
 const Api = axios.create({
-
   baseURL:APIURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    "token":localStorage.getItem("token")
   },
 });
 export default Api;
